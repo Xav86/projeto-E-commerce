32def0897621949f0d45a3e42d92d84eac17e24d
Xav86 <gustavo.g.2005@gmail.com>
2024-08-30 14:19:35 -0300

    feito base da API do projeto

---
+++ programa/server/README.md
@@ -0,0 +1,3 @@
+# Api do projeto E-commerce
+
+essa api vai ser uma API Rest (não restfull) apenas para fazer as requisições para o banco conforme o necessário, conforme o necessário irei estar criando as rotas.
---
+++ programa/server/src/index.js
@@ -0,0 +1,15 @@
+/* Raiz do projeto, onde vai ser chamado as rotas e configurações no geral  */
+const express = require('express');
+const app = express();
+const router = require('./routes/routes');
+require("dotenv").config();
+
+app.use(express.json());
+
+// Static
+app.use(express.static('public'));
+
+/* Rotas */
+app.use("/", router);
+
+module.exports = app;
