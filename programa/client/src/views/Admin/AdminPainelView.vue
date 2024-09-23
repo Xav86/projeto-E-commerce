cffdfbd6fe60ed6f057e0b993bbfb2f22df13635
Xav86 <gustavo.g.2005@gmail.com>
2024-09-23 17:45:48 -0300

    começando a trabalhar na visualização de usuários/ remodelando painel administrativo

--- programa/client/src/views/Admin/AdminPainelView.vue
+++ programa/client/src/views/Admin/AdminPainelView.vue
@@ -1,76 +1,16 @@
 <template>
-    <div  class="products">
-        <NavbarComponent />
-        <div class="container">
-            <div class="title-new">
-                <h1>Listagem de Produtos</h1>
-                <div><router-link to="/admin/new-product" class="btn btn-success">Novo Jogo</router-link></div>
-            </div>
-            <hr>
-            <ul class="products-list">
-                <li v-for="i in 10" :key="i">
-                    <CardItemListAdmin />
-                </li>
-            </ul>
-        </div>
-        <ModalEditGame />
-        <FooterComponent />
-    </div>
+  <div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate deserunt necessitatibus, repellat inventore ab aperiam quasi assumenda ex expedita, facilis accusamus iusto eius autem modi voluptas debitis nostrum esse ullam!</p>
+    <p>trabalhando nisso</p>
+  </div>
 </template>
 
 <script>
-import CardItemListAdmin from '@/components/CardItens/CardItemListAdmin.vue';
-import FooterComponent from '@/components/footer/footerComponent.vue';
-import NavbarComponent from '@/components/navbar/NavbarComponent.vue';
-import ModalEditGame from '@/components/modal/ModalEditGame.vue';
-
 export default {
-    components: {
-      NavbarComponent,
-      FooterComponent,
-      CardItemListAdmin,
-      ModalEditGame
-    }
+
 }
 </script>
 
-<style scoped>
-    .title-new {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-    }
+<style>
 
-    .admin-painel {
-      min-height: 74.6vh;
-    }
-
-    .products {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .container {
-        text-align: center;
-        min-height: 74.6vh;
-        margin-bottom: 17px;
-    }
-
-    .products li {
-        list-style-type:none;
-        padding: 0;
-    }
-
-    .products ul {
-        margin: 0;
-        padding: 0;
-    }
-
-    .products-list {
-        display: flex;
-        flex-direction: column;
-        gap: 17px;
-    }
 </style>
