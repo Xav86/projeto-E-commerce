a45ea6bdc681e6f994cc0aa9062cb13e1416a69e
Xav86 <gustavo.g.2005@gmail.com>
2024-09-24 17:58:10 -0300

    iniciando estilo da pagina

--- programa/client/src/views/Admin/AdmUserList.vue
+++ programa/client/src/views/Admin/AdmUserList.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maiores quod id, nulla, omnis sed voluptatem rem laboriosam laudantium delectus eum! Vitae quia, atque voluptatibus excepturi commodi expedita magnam accusantium.</p>
-  </div>
+    <div class="background">
+        <div class="container-list-user">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint maiores quod id, nulla, omnis sed voluptatem rem laboriosam laudantium delectus eum! Vitae quia, atque voluptatibus excepturi commodi expedita magnam accusantium.</p>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,5 +13,18 @@ export default {
 </script>
 
 <style>
+    .background {
+        background-image: url('@/assets/images/retro-background-adm.png');
+        background-repeat: no-repeat;
+        background-size: cover;
+        background-position-x: center;
+    }
 
+    .container-list-user {
+        width: 100%;
+        min-height: 100vh;
+
+        background-color: #000000a3;
+        color: #fff;
+    }
 </style>
